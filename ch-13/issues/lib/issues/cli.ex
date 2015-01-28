ae4e0e8548fb6d33d520c960c77adb3c823148ff
Lauro Caetano <laurocaetano1@gmail.com>
2015-01-28 18:35:32 -0200

    Decode and sort the github's json of issues.

--- ch-13/issues/lib/issues/cli.ex
+++ ch-13/issues/lib/issues/cli.ex
@@ -39,5 +39,23 @@ defmodule Issues.CLI do
 
   def process({{user, project, _count}}) do
     Issues.GithubIssues.fetch(user, project)
+    |> decode_response
+    |> convert_to_list_of_hashdicts
+    |> sort_into_ascending_order
+  end
+
+  def decode_response({:ok, body}), do: body
+  def decode_response({:error, error}) do
+    {_, message} = List.keyfind(error, "message", 0)
+    IO.puts "Error fetching from Github: #{message}"
+    System.halt(2)
+  end
+
+  def convert_to_list_of_hashdicts(list) do
+    Enum.map(list, &Enum.into(&1, HashDict.new))
+  end
+
+  def sort_into_ascending_order(list_of_issues) do
+    Enum.sort list_of_issues, &(&1["created_at"] <= &2["created_at"])
   end
 end
--- ch-13/issues/mix.exs
+++ ch-13/issues/mix.exs
@@ -12,7 +12,7 @@ defmodule Issues.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :httpoison]]
   end
 
   # Dependencies can be Hex packages:
@@ -26,7 +26,8 @@ defmodule Issues.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [
-      {:httpoison, "~> 0.4"}
+        {:httpoison, "~> 0.4"},
+        {:jsx, "~> 2.0"}
     ]
   end
 end
