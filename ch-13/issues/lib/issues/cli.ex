241304c328f908da349fbf739725a61185790c6b
Lauro Caetano <laurocaetano1@gmail.com>
2015-03-06 14:20:30 -0300

    Add table formatter

--- ch-13/issues/lib/issues/cli.ex
+++ ch-13/issues/lib/issues/cli.ex
@@ -37,11 +37,12 @@ defmodule Issues.CLI do
     System.halt(0)
   end
 
-  def process({{user, project, _count}}) do
+  def process({{user, project, count}}) do
     Issues.GithubIssues.fetch(user, project)
     |> decode_response
     |> convert_to_list_of_hashdicts
     |> sort_into_ascending_order
+    |> Enum.take(count)
   end
 
   def decode_response({:ok, body}), do: body
