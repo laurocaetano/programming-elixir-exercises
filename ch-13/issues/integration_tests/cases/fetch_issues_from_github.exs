2d3fc4ff662568ce7eb6181308f3ab0c3eff79c3
Lauro Caetano <laurocaetano1@gmail.com>
2015-03-26 15:38:44 -0300

    Add integration tests

--- ch-13/issues/integration_tests/cases/fetch_issues_from_github.exs
+++ ch-13/issues/integration_tests/cases/fetch_issues_from_github.exs
@@ -1,3 +1,15 @@
 defmodule Issues.Integration.FetchTest do
   use ExUnit.Case, async: true
+  import ExUnit.CaptureIO
+
+  test "gets user information from github" do
+    assert capture_io(fn ->
+      Issues.CLI.process({{"elixir-lang", "elixir", 3}})
+    end) ==
+    ~s{numb | created_at           | title                             \n} <>
+    ~s{-----+----------------------+-----------------------------------\n} <>
+    ~s{3043 | 2015-02-02T17:43:26Z | Added man page for iex/elixir     \n} <>
+    ~s{3096 | 2015-02-17T15:35:14Z | Module resolution at compile time \n} <>
+    ~s{3100 | 2015-02-19T15:58:34Z | Move logger flushing to Kernel CLI\n}
+  end
 end
